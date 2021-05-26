9b7c905d36d5570aafb2c9597d308e6d610a52ef
DELL <amine.m.a1998@gmail.com>
2021-05-26 01:11:57 +0100

    add card

--- src/Pages/home/allNotes/index.jsx
+++ src/Pages/home/allNotes/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MainContent from '../../../Layouts/main/index'
 import NavBar from '../../../Layouts/navbar/NavBar'
+import './style.css'
 
 class AllNotes extends Component {
 
@@ -8,7 +9,25 @@ class AllNotes extends Component {
         super(props);
         this.state = { 
             notes : [
-                {},
+                {
+                    title :'title',
+                    image :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7flKz7FNaNYsIfRuYmfNADqWYJrKTEse--g&usqp=CAU' ,
+                    description :'A note is a short document that has to be signed by someone and that gives official information about something' ,
+
+                },
+                {
+                    title :'title',
+                    image :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7flKz7FNaNYsIfRuYmfNADqWYJrKTEse--g&usqp=CAU' ,
+                    description :'description ' ,
+
+                },
+                {
+                    title :'title',
+                    image :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7flKz7FNaNYsIfRuYmfNADqWYJrKTEse--g&usqp=CAU' ,
+                    description :'description ' ,
+
+                },
+               
             ]
          }
     }
@@ -18,6 +37,17 @@ class AllNotes extends Component {
             <NavBar isAuthorized={true} />   
             <MainContent>
                 <h1>Notes</h1>
+                <section>
+                {
+                    this.state.notes.map(note  => (
+                    <article>
+                        <img src={note.image} />
+                        <h1>{note.title}</h1>
+                        <p>{note.description.slice(0,100)} .....</p>
+                        <button type="button" >More</button>
+                    </article>) ) 
+                }
+                </section>
             </MainContent>
         </div> );
     }
